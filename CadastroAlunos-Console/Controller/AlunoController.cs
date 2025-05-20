fba4f8c1c7eb99fd1b72184c0387d9250a7e394a
Júlia Santos <juliagalvaoalmeidasantos@gmail.com>
2025-05-19 22:10:28 -0300

    criando "CadastrarAluno", "ListarAluno", "AtualizarAluno" e "ExcluirAlunos"

--- CadastroAlunos-Console/Controller/AlunoController.cs
+++ CadastroAlunos-Console/Controller/AlunoController.cs
@@ -17,5 +17,29 @@ namespace CadastroAlunos_Console.Controller
             Aluno novoAluno = new(proximoId++, nome, cpf, curso, dataNascimento);
             alunos.Add(novoAluno);
         }
+
+        public List<Aluno>  ListarAlunos()
+        { 
+            return alunos;
+        }
+
+        public bool AtualizarAluno(int id, string nome, string cpf, string curso, DateTime dataNascimento)
+        {
+            Aluno aluno = alunos.Find(a => a.Id == id);
+            if (aluno == null) return false;
+
+            aluno.Nome = nome;
+            aluno.CPF = cpf;
+            aluno.Curso = curso;
+            aluno.DataNascimento = dataNascimento;
+            
+            return true;
+        }
+        
+        public bool ExcluirAluno(int id)
+        {
+            Aluno aluno = alunos.Find(a => a.Id == id);
+            return (aluno != null && alunos.Remove(aluno));
+        }
     }
 }
--- CadastroAlunos-Console/Views/AlunoView.cs
+++ CadastroAlunos-Console/Views/AlunoView.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CadastroAlunos_Console.Controller;
+using CadastroAlunos_Console.Models;
 using Microsoft.VisualBasic;
 
 namespace CadastroAlunos_Console.Views
@@ -32,23 +33,24 @@ namespace CadastroAlunos_Console.Views
                         CadastrarAluno();
                         break;
 
-                    //case 2:
-                    //    ListarAluno();
-                    //    break;
+                    case 2:
+                        ListarAluno();
+                        break;
 
-                    //case 3:
-                    //    AtualizarAlunos();
-                    //    break;
+                    case 3:
+                        AtualizarAluno();
+                        break;
 
-                    //case 4:
-                    //    ExcluirAlunos();
-                    //    break;
+                    case 4:
+                        ExcluirAlunos();
+                        break;
                 }
                 Console.WriteLine("\nPressione Enter para continuar...");
                 Console.ReadLine();
 
             } while (opcao != 0);
         }
+
         private void CadastrarAluno()
         {
             Console.WriteLine("Nome: ");
@@ -63,5 +65,42 @@ namespace CadastroAlunos_Console.Views
             controller.CadastrarAluno(nome, cpf, curso, data);
             Console.WriteLine("Aluno cadastrado com sucesso!");
         }
+
+        private void ListarAluno()
+        {
+            List<Aluno> alunos = controller.ListarAlunos();
+            Console.WriteLine("\n lista de alunos: ");
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine($"ID: {aluno.Id} | Nome: {aluno.Nome} | CPF: {aluno.CPF} " +
+                    $"| Curso: {aluno.Curso} | Data de nascimento: {aluno.DataNascimento}");
+            }
+        }
+
+        private void AtualizarAluno()
+        {
+            Console.WriteLine("Digite o ID do aluno a ser atualizado: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Novo nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Novo CPF: ");
+            string cpf = Console.ReadLine();
+            Console.WriteLine("Novo curso: ");
+            string curso = Console.ReadLine();
+            Console.WriteLine("Nova data de nascimento: ");
+            DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+            bool atualizado = controller.AtualizarAluno(id, nome, cpf, curso, dataNascimento);
+            Console.WriteLine(atualizado ? "Aluno atualizado com sucesso!" : "Aluno não encontrado.");
+        }
+
+        private void ExcluirAlunos()
+        {
+            Console.WriteLine("Digite o ID do aluno a ser excluído: ");
+            int id = int.Parse(Console.ReadLine());
+            bool excluido = controller.ExcluirAluno(id);
+            Console.WriteLine(excluido ? "Aluno excluído com sucesso!" : "Aluno não encontrado.");
+        }
     }
 }
+
